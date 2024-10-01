fa7ea84ee7f0be38a684866cbe6ec9504d5e7e64
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-10-01 15:06:08 +0530

    My first commit

---
+++ student_details.py
@@ -0,0 +1,7 @@
+name=input("Enter Student Name:")
+roll_no=int(input("Enter your roll no:"))
+cgpa=float(input("enter you're cgpa:"))
+percentage=cgpa*10
+print("you're name is",name)
+print("you're roll no is",roll_no)
+print("your're total percentage is",percentage,"%")

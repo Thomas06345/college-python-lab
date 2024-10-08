05131e61715a535233a0f88cc717529bc276a66c
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-10-08 15:05:05 +0530

    Add files via upload

---
+++ concatenate, and print.py
@@ -0,0 +1,10 @@
+'''Author:Thomas Cherian
+   date: 8-10-24
+   python program:to extract a concatenated sting'''
+first_name=input("enter your first name")
+last_name=input("enter your last name")
+full_name=first_name+" "+last_name
+print(full_name)
+length=len(first_name)
+extracted_first_name=full_name[:length]
+print(extracted_first_name)

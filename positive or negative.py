dd02353be7875427d273de50ae072b9770e27850
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-10-15 14:03:40 +0530

    To check whether the given number is positive or not

---
+++ positive or negative.py
@@ -0,0 +1,8 @@
+'''Author:Thomas Cherian
+   date: 15-10-24
+   python program:to check whether the given number is positive or not'''
+number=int(input("Enter a number:"))
+if number>0:
+    print(":The given number",number," is positive")
+else:
+        print("The given number",number," is negative")

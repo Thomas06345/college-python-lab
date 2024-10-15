2f02c71a2d1c7fcd75655268b7708be61c8467f8
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-10-15 15:06:58 +0530

    to determine if the user is eligible or not

---
+++ driving license.py
@@ -0,0 +1,8 @@
+'''Author:Thomas Cherian
+   date: 15-10-24
+   python program:to determine if user is eligible for driving license '''
+age=int(input("Enter your age"))
+if age >= 16:
+    print("Eligible for driving license")
+else:
+    print("Not eligible for driving license")

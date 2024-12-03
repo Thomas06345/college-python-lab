f55c1c27a8f402348842093325b2d5000e62872f
Thomas06345 <thomascherian2028@cs.sjcetpalai.ac.in>
2024-12-03 15:21:39 +0530

    To create a fibonacci series using a user generated function

---
+++ fibonacci_generator.py
@@ -0,0 +1,6 @@
+'''Author:Thomas Cherian
+   date: 3-12-24
+   python program:To create a fibonacci series using a user generated function '''
+import fibonacci as fib
+num=int(input("Enter the number:"))
+print(fib.fibonacci(num))
